fe7f2bfd4b596eeaba74e226175cb1094ebd4bfd
wangli <1821180991@qq.com>
2019-09-11 11:16:14 +0800

    完成项目的静态页面

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,5 @@
 const { override, fixBabelImports, addLessLoader ,addDecoratorsLegacy,addWebpackAlias } = require('customize-cra');
-
+const{resolve}=require('path');
 module.exports = override(
     //按需加载组件代码和样式
     fixBabelImports('import', {
@@ -16,6 +16,10 @@ module.exports = override(
     addDecoratorsLegacy(),
     //配置路径别名：简化路径(问题：路径没有提示)
     addWebpackAlias({
+       '@comps':resolve(__dirname,'src/components'),
+       '@conts':resolve(__dirname,'src/containers'),
+       '@config':resolve(__dirname,'src/config'),
+       '@redux':resolve(__dirname,'src/redux'),
 
     })
 );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
+
 import store from './redux/store'
 import App from './App';
+
+import './assets/less/index.less';
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
