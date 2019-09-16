6be4873dca5385b20c5bbfc2add2de060433ece8
wangli <1821180991@qq.com>
2019-09-16 10:05:48 +0800

    全屏展示和搜索就已经完成了

--- src/components/basic-layout/header-main/index.jsx
+++ src/components/basic-layout/header-main/index.jsx
@@ -1,13 +1,38 @@
 import React, {Component} from 'react';
 import {Button,Icon} from 'antd';
+import screenfull from 'screenfull';
 import './index.less';
 
 class HeaderMain extends Component {
+    state={
+        isScreenFull:false
+    };
+    screenFull=()=> {
+        if (screenfull.isEnabled){
+            screenfull.toggle();
+
+        }
+    };
+    change=()=> {
+        this.setState({
+            isScreenFull:!this.state.isScreenFull
+        })
+    };
+    componentDidMount() {
+        //绑定事件
+        screenfull.on('change',this.change);
+    }
+    componentWillUnmount() {
+        //解绑事件
+        screenfull.off('change',this.change);
+    }
+
     render() {
+        const {isScreenFull}=this.state;
         return (
             <div className="header-main">
                 <div className="header-main-top">
-                    <Button size='small'><Icon type="fullscreen"/></Button>
+                    <Button size='small' onClick={this.screenFull}><Icon type={isScreenFull ? 'fullscreen-exit':'fullscreen'}/></Button>
                     <Button size='small' className="header-main-btn">English</Button>
                     <span>欢迎，xxx</span>
                     <Button type="link">退出</Button>
